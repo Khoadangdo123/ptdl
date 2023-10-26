82dbe421b3af04c9a345c0529eaf1d31f53495ac
Khoa <90438735+Khoadangdo123@users.noreply.github.com>
2023-10-27 05:13:47 +0700

    update version 1

--- PTDLJS/src/App.jsx
+++ PTDLJS/src/App.jsx
@@ -10,11 +10,24 @@ function App() {
   const [columnData, setColumnData] = useState('');
   const [dataValue, setDataValue] = useState('');
   const [dataNew, setDataNew] = useState('');
+
   const [csvData, setCsvData] = useState(null);
   const [csvDataNeed, setCsvDataNeed] = useState(null);
+  const [csvDataUpdated, setCsvDataUpdated] = useState(null)
 
   const [nameData, setNameData] = useState('');
   const [nameDataChange, setNameDataChange] = useState('');
+  const [updatedNameDataChange, setUpdatedNameChange] = useState('');
+
+  const [updatedColumnData, setUpdatedColumnData] = useState('');
+  const [updatedDataValue, setUpdatedDataValue] = useState('');
+  const [updatedDataNew, setUpdatedDataNew] = useState('');
+
+  const [deleteDataChange, setDeleteDataChange] = useState('');
+  const [deleteNameDataChange, setDeleteNameDataChange] = useState('');
+  const [deleteColumn, setDeleteColumn] = useState('')
+  const [fileDelete, setFileDelete] = useState(null);
+
 
   const handleFileUpload = (e) => {
     const file = e.target.files[0];
@@ -51,6 +64,61 @@ function App() {
 
     reader.readAsText(file);
   };
+
+  const handleFileUpdatedUploadChange = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    setUpdatedNameChange(file.name)
+
+    reader.onload = (event) => {
+      const text = event.target.result;
+      Papa.parse(text, {
+        complete: (result) => {
+          setCsvDataUpdated(result.data);
+        },
+      });
+    };
+
+    reader.readAsText(file);
+  }
+
+  const handleFileDeleteChange = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    // setDeleteNameDataChange(file.name)
+    setDeleteNameDataChange(file.name)
+
+    reader.onload = (event) => {
+      const text = event.target.result;
+      Papa.parse(text, {
+        complete: (result) => {
+          setFileDelete(result.data);
+        },
+      });
+    };
+
+    reader.readAsText(file);
+  }
+
+  const deleteValue = () => {
+
+    const formData = new FormData();
+
+    formData.append('file', new Blob([Papa.unparse(fileDelete)], { type: 'text/csv' }), deleteNameDataChange);
+    formData.append('dataColoumn', deleteColumn);
+    formData.append('dataDelete', deleteDataChange);
+
+    axios.post('http://localhost:3001/deleteValue', formData)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error('Error adding column', error)
+      })
+
+  }
 
   const generateRandomValue = () => {
     // Generate a random value (you can customize this based on your requirements)
@@ -92,6 +160,25 @@ function App() {
     formData.append('dataValue', dataValue);
     formData.append('dataNew', dataNew);
     formData.append('dataLength', csvDataNeed.length - 1);
+
+    axios.post('http://localhost:3001/addValue', formData)
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error('Error adding column:', error);
+        });
+  };
+
+  const updatedValue = () => {
+
+    const formData = new FormData();
+
+    formData.append('file', new Blob([Papa.unparse(csvDataUpdated)], { type: 'text/csv' }), updatedNameDataChange);
+    formData.append('dataColoumn', updatedColumnData);
+    formData.append('dataValue', updatedDataValue);
+    formData.append('dataNew', updatedDataNew);
+    formData.append('dataLength', csvDataUpdated.length - 1);
 
     axios.post('http://localhost:3001/addValue', formData)
         .then(response => {
@@ -163,94 +250,283 @@ function App() {
         </div>
 
         <br />
-        {/* Them gia tri vao cot va file */}
-        <h1>
-          Thêm giá trị vào cột cần thêm
-        </h1>
+        {/* Them gia tri vao cot va file vaf cap nhat file */}
         <div style={{
-          width: 500,
           display: 'flex',
-          justifyContent: 'space-around'
+          width: '100%'
         }}>
-          <h5
+          <div 
             style={{
-              marginLeft: 40
+            width: '50%'
+          }}>
+            <h1>
+              Thêm giá trị vào cột cần thêm
+            </h1>
+            <div style={{
+              width: 500,
+              display: 'flex',
+              justifyContent: 'space-around'
+            }}>
+              <h5
+                style={{
+                  marginLeft: 40
+                }}
+              >
+                File cần giá trị
+              </h5>
+              <div style={{
+                textAlign: 'center'
+              }}>
+                <input
+                  type="file"
+                  onChange={handleFileUploadChange}
+                  style={{
+                    marginTop: 23
+                  }}
+                />
+              </div>
+            </div>
+            <br />
+            <div style={{
+              width: 1000,
+              display: 'flex',
+              justifyContent: 'space-around'
+            }}>
+              <span>
+                Nhập tên cột cần thêm
+              </span>
+              <input
+                className='input_add_value'
+                placeholder='Tên cột mới'
+                type="text"
+                onChange={(e) => setColumnData(e.target.value)}
+              />
+            </div>
+            <br />
+            <div style={{
+              width: 1000,
+              display: 'flex',
+              justifyContent: 'space-around'
+            }}>
+              <span>
+                Nhập Số Id cần thêm
+              </span>
+              <input
+                className='input_add_value'
+                placeholder='Tên id cần thêm'
+                type="text"
+                onChange={(e) => setDataValue(e.target.value)}
+              />
+            </div>
+            <br />
+            <div style={{
+              width: 1000,
+              display: 'flex',
+              justifyContent: 'space-around'
+            }}>
+              <span>
+                Nhập giá trị cần nhập
+              </span>
+              <input
+                className='input_add_value'
+                placeholder='Tên giá trị cần nhập'
+                type="text"
+                onChange={(e) => setDataNew(e.target.value)}  
+              />
+            </div>
+            <br />
+            <button
+              className='hover_button_add_value'
+              style={{
+                marginLeft: 80,
+                background: '#5D12D2',
+                color: 'white'
+              }}
+              onClick={addValue}
+            >
+              Thêm giá giá trị
+            </button>
+          </div>
+          <div
+            style={{
+              width: '50%'
             }}
           >
-            File cần giá trị
-          </h5>
-          <div style={{
-            textAlign: 'center'
-          }}>
-            <input
-              type="file"
-              onChange={handleFileUploadChange}
+            <h1
               style={{
-                marginTop: 23
+                color: 'blue'
               }}
-            />
+            >
+              Cập nhật giá trị cột cần
+            </h1>
+            <div style={{
+              width: 500,
+              display: 'flex',
+              justifyContent: 'space-around'
+            }}>
+              <h5
+                style={{
+                  marginLeft: 40
+                }}
+              >
+                File cần cập nhật
+              </h5>
+              <div style={{
+                textAlign: 'center'
+              }}>
+                <input
+                  type="file"
+                  onChange={handleFileUpdatedUploadChange}
+                  style={{
+                    marginTop: 23
+                  }}
+                />
+              </div>
+            </div>
+            <br />
+            <div style={{
+              width: 1000,
+              display: 'flex',
+              justifyContent: 'space-around'
+            }}>
+              <span>
+                Nhập tên cột cần cập nhật
+              </span>
+              <input
+                className='input_add_value'
+                placeholder='Tên cột cập nhật'
+                type="text"
+                onChange={(e) => setUpdatedColumnData(e.target.value)}
+              />
+            </div>
+            <br />
+            <div style={{
+              width: 1000,
+              display: 'flex',
+              justifyContent: 'space-around'
+            }}>
+              <span>
+                Nhập Số Id cần cập nhật
+              </span>
+              <input
+                className='input_add_value'
+                placeholder='Tên id cần cập nhật'
+                type="text"
+                onChange={(e) => setUpdatedDataValue(e.target.value)}
+              />
+            </div>
+            <br />
+            <div style={{
+              width: 1000,
+              display: 'flex',
+              justifyContent: 'space-around'
+            }}>
+              <span>
+                Nhập giá trị cần cập nhật
+              </span>
+              <input
+                className='input_add_value'
+                placeholder='Tên giá trị cần cập nhật'
+                type="text"
+                onChange={(e) => setUpdatedDataNew(e.target.value)}  
+              />
+            </div>
+            <br />
+            <button
+              className='hover_button_add_value'
+              style={{
+                marginLeft: 80,
+                background: '#02c8f9',
+                color: 'black'
+              }}
+              onClick={updatedValue}
+            >
+              Cập nhật giá trị
+            </button>
           </div>
         </div>
-        <br />
-        <div style={{
-          width: 1000,
-          display: 'flex',
-          justifyContent: 'space-around'
-        }}>
-          <span>
-            Nhập tên cột cần thêm
-          </span>
-          <input
-            className='input_add_value'
-            placeholder='Tên cột mới'
-            type="text"
-            onChange={(e) => setColumnData(e.target.value)}
-          />
-        </div>
-        <br />
-        <div style={{
-          width: 1000,
-          display: 'flex',
-          justifyContent: 'space-around'
-        }}>
-          <span>
-            Nhập Số Id cần thêm
-          </span>
-          <input
-            className='input_add_value'
-            placeholder='Tên id cần thêm'
-            type="text"
-            onChange={(e) => setDataValue(e.target.value)}
-          />
-        </div>
-        <br />
-        <div style={{
-          width: 1000,
-          display: 'flex',
-          justifyContent: 'space-around'
-        }}>
-          <span>
-            Nhập giá trị cần nhập
-          </span>
-          <input
-            className='input_add_value'
-            placeholder='Tên giá trị cần nhập'
-            type="text"
-            onChange={(e) => setDataNew(e.target.value)}  
-          />
-        </div>
-        <br />
-        <button
-          className='hover_button_add_value'
+      </div>
+      <div>
+        <h1
           style={{
-            marginLeft: 80,
-            background: '#5D12D2',
-            color: 'white'
+            color: 'red'
           }}
-          onClick={addValue}
         >
-          Thêm giá giá trị
-        </button>
+          Giá trị cột cần xóa
+        </h1>
+        <div>
+          <div
+            style={{
+              display: 'flex'
+
+            }}
+          >
+            <h5
+              style={{
+                marginLeft: 40
+              }}
+            >
+              File cần xóa
+            </h5>
+            <div>
+              <input
+                type="file"
+                onChange={handleFileDeleteChange}
+                style={{
+                  marginTop: 23,
+                  marginLeft: 20
+                }}
+              />
+            </div>
+          </div>
+          <div
+            style={{
+              marginLeft: 40,
+            }}
+          >
+            <span>Cột cần xóa</span>
+            <input
+              className='input_chosen_file'
+              style={{
+                marginLeft: 30,
+                height: 24
+              }}
+              type="text"
+              onChange={(e) => setDeleteColumn(e.target.value)}
+              placeholder="Tên cột xóa"
+            />
+          </div>
+          <div
+            style={{
+              marginLeft: 40,
+              marginTop: 20
+            }}
+          >
+            <span>Id cần xóa</span>
+            <input
+              className='input_chosen_file'
+              style={{
+                marginLeft: 41,
+                height: 24
+              }}
+              type="text"
+              onChange={(e) => setDeleteDataChange(e.target.value)}
+              placeholder="Tên id xóa"
+            />
+          </div>
+          
+          <button
+            style={{
+              background: 'red',
+              color: 'white',
+              marginLeft: 40,
+              marginTop: 20
+            }}
+            onClick={deleteValue}
+          >
+            Delete
+          </button>
+        </div>
       </div>
     </>
   );
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
 const Papa = require('papaparse');
+const csv = require('csv-parser');
 const cors = require('cors');
 
 const app = express();
@@ -83,7 +84,61 @@ app.post('/addValue', upload.single('file'), (req, res) => {
 	// fs.unlinkSync(filePath);
 
 	res.send(parsedData)
-})
+});
+
+app.post('/deleteValue', upload.single('file'), (req, res) => {
+	const file = req.file;
+
+	if (!file) {
+    res.status(400).send('No file uploaded');
+    return;
+  }
+	const filePath = `uploads/${file.originalname}`;
+	const data = fs.readFileSync(filePath, 'utf8');
+  const parsedData = Papa.parse(data, { header: true });
+
+	const newData = parsedData.data.map((row, index) => {
+		if (row['Order ID'] === req.body.dataDelete) {
+			return (
+				{
+					...row,
+					[req.body.dataColoumn]: ''
+				}
+			)
+		} else {
+			return row;
+		}
+	});
+
+  const newCSV = Papa.unparse(newData);
+	fs.writeFileSync(filePath, newCSV);
+	// fs.unlinkSync(filePath);
+
+	res.send(parsedData)
+});
+
+// app.post('/deleteRow', upload.single('file'), (req, res) => {
+// 	const uploadedFile = req.file;
+// 	const columnNameToDelete = req.body.columnName;
+
+//   const rows = [];
+// 	let header = null;
+// 	fs.createReadStream(uploadedFile.path) // Provide the correct path to your CSV file
+// 		.pipe(csv())
+// 		.on('data', (row) => {
+// 			if (!header) {
+//         header = Object.keys(row);
+//       }
+//       delete row[columnNameToDelete];
+//       rows.push(row);
+// 		})
+// 		.on('end', () => {
+// 			const csvData = [header, ...rows.map(row => Object.values(row))];
+// 			fs.writeFileSync('data.csv', csvData.map(row => row.join(',')).join('\n')); // Write back to the CSV file
+// 			res.json(csvData.map(row =>  row.join(',')))
+// 			// res.json({ message: `Column "${columnNameToDelete}" deleted successfully` });
+// 		});
+// });
 
 app.listen(3001, () => {
   console.log('Server is running on http://localhost:3001');
