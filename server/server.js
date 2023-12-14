6160ec1e4a7e0b6778ac905b3c65201f791c2a30
Khoa <90438735+Khoadangdo123@users.noreply.github.com>
2023-12-14 12:51:21 +0700

    first commit

--- server/server.js
+++ server/server.js
@@ -3,7 +3,11 @@ const multer = require('multer');
 const fs = require('fs');
 const Papa = require('papaparse');
 const csv = require('csv-parser');
+const brain = require('brain.js');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const { PythonShell } = require('python-shell');
+const tf = require('@tensorflow/tfjs');
 
 const app = express();
 
@@ -236,6 +240,226 @@ app.post('/deleteValue', upload.single('file'), (req, res) => {
 // 			// res.json({ message: `Column "${columnNameToDelete}" deleted successfully` });
 // 		});
 // });
+
+app.post('/train', bodyParser.json(), (req, res) => {
+	
+	const trainingData = [];
+	const newPriceEach = parseFloat(req.body.newPrice); 
+
+	console.log(newPriceEach)
+
+	fs.createReadStream('./uploads/predict.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    const priceEach = parseFloat(row['Price Each']);
+    const quantityOrdered = parseFloat(row['Quantity Ordered']);
+
+    trainingData.push({ input: { priceEach }, output: { quantityOrdered } });
+  })
+  .on('end', () => {
+    // Create and configure a neural network
+    const net = new brain.NeuralNetwork({ hiddenLayers: [3] });
+
+    // Train the neural network
+    net.train(trainingData, {
+      iterations: 1000,
+      log: true, // Log tiến trình đào tạo vào console
+      errorThresh: 0.005,  // Dừng đào tạo khi sai số đạt ngưỡng này
+      logPeriod: 100, // Log tiến trình mỗi 100 lần lặp
+    });
+
+    // Make predictions for new data
+    const output = net.run({ priceEach: newPriceEach });
+
+    res.json({
+			valuePredict: output.quantityOrdered
+		});
+  });
+});
+
+app.post('/trainTongGiaTien', bodyParser.json(), (req, res) => {
+	
+	const trainingData = [];
+	// const newPriceEach = parseFloat(req.body.newPrice); // Replace with your new input
+	const valueQuantityName = parseInt(req.body.predictQuantity);
+
+	console.log(valueQuantityName)
+
+	// console.log()
+
+	fs.createReadStream('./uploads/predict.csv')
+	.pipe(csv())
+	.on('data', (row) => {
+		const sumEach = parseFloat(row['Price Each']) * parseFloat(row['Quantity Ordered']);
+		const quantityOrdered = parseFloat(row['Quantity Ordered']);
+
+		// Check for NaN or undefined values in numeric attributes
+		if (!isNaN(sumEach) && !isNaN(quantityOrdered)) {
+			trainingData.push({
+				input: {
+					quantityOrdered,
+					// Add more attributes here
+				},
+				output: {
+					sumEach,
+					// Add more outputs here
+				},
+			});
+		} else {
+			console.warn('Invalid or missing numeric values:', row);
+		}
+	})
+	.on('end', () => {
+		// Create and configure a neural network
+		const net = new brain.NeuralNetwork({ hiddenLayers: [3] });
+
+		// Check if there is valid training data
+		if (trainingData.length > 0) {
+			// Train the neural network
+			net.train(trainingData, {
+				learningRate: 0.01,
+				iterations: 1000,
+				log: true,
+				errorThresh: 0.005,
+				logPeriod: 100
+			});
+
+			// Make predictions for new data
+			const output = net.run({ quantityOrdered: valueQuantityName });
+
+			res.json({
+				valuePredict: output.sumEach,
+				// Add more predicted values here
+			});
+		} else {
+			res.status(500).json({ error: 'No valid training data.' });
+		}
+	});
+});
+
+app.post('/trainDiaChi', bodyParser.json(), (req, res) => {
+	const trainingData = [];
+
+	const valueProductNameData = req.body.valueProductPredictNew;
+// Load your CSV file and populate training data
+	fs.createReadStream('./uploads/predict.csv')
+		.pipe(csv())
+		.on('data', (row) => {
+			const product = row['Product'];
+			const purchaseAddress = row['Purchase Address'];
+
+			// Convert the strings to arrays of characters
+			const inputArray = product.split('');
+			const outputArray = purchaseAddress.split('');
+
+			trainingData.push({
+				input: inputArray,
+				output: outputArray,
+			});
+		})
+		.on('end', () => {
+			// Create and configure an LSTM neural network
+			const net = new brain.recurrent.LSTM();
+
+			// Check if there is valid training data
+			if (trainingData.length > 0) {
+				net.train(trainingData, {
+					learningRate: 0.01,
+					iterations: 1000,
+					log: true,
+					errorThresh: 0.005,
+					logPeriod: 100,
+				});
+
+				// Make predictions for new data
+				// const sampleProduct = valueProductNameData;
+				const inputArray = valueProductNameData.split('');
+				// console.log(inputArray)
+				const output = net.run(inputArray);
+
+				// Convert the output array to a string
+				// const predictedPurchaseAddress = Object.keys(output).reduce((maxKey, key) => (output[key] > output[maxKey] ? key : maxKey), '');
+
+				// console.log('Predicted Purchase Address:', output);
+				res.json({
+					valuePredict: output
+				})
+			} else {
+				console.error('No valid training data.');
+			}
+		});
+});
+
+app.post('/trainSanPham', bodyParser.json(), (req, res) => {
+	
+// Function to decode lengths back to categorical values
+	function decodeLength(encodedLength, categoryArray) {
+		return categoryArray[encodedLength];
+	}
+
+	// Read data from CSV file and train the model
+	const trainingData = [];
+	const productCategories = []; // Assuming 'Product' is a categorical variable
+	const purchaseAddressCategories = []; // Assuming 'Purchase Address' is a categorical variable
+
+	const inputNewPriceEach = parseFloat(req.body.newPriceEach);
+
+
+	fs.createReadStream('./uploads/predict.csv')
+		.pipe(csv())
+		.on('data', (row) => {
+			const priceEach = parseFloat(row['Price Each']);
+			const product = row['Product'];
+			const purchaseAddress = row['Purchase Address'];
+
+			// Populate category arrays
+			if (!productCategories.includes(product)) {
+				productCategories.push(product);
+			}
+
+			if (!purchaseAddressCategories.includes(purchaseAddress)) {
+				purchaseAddressCategories.push(purchaseAddress);
+			}
+
+			trainingData.push({
+				input: { priceEach },
+				output: { product: productCategories.indexOf(product), purchaseAddress: purchaseAddressCategories.indexOf(purchaseAddress) },
+			});
+		})
+		.on('end', () => {
+			// Convert training data to TensorFlow tensors
+			const xs = tf.tensor2d(trainingData.map(data => [data.input.priceEach]));
+			const ys = tf.tensor2d(trainingData.map(data => [data.output.product, data.output.purchaseAddress]));
+
+			// Define the model
+			const model = tf.sequential();
+			model.add(tf.layers.dense({ units: 10, inputShape: [1], activation: 'relu' }));
+			model.add(tf.layers.dense({ units: 2, activation: 'softmax' }));
+
+			// Compile the model
+			model.compile({ optimizer: 'adam', loss: 'categoricalCrossentropy', metrics: ['accuracy'] });
+
+			// Train the model
+			model.fit(xs, ys, { epochs: 100 }).then(() => {
+				// Model has been trained
+				// You can make predictions or evaluate the model here
+
+				// Make predictions for new data
+				const newPriceEach = inputNewPriceEach;
+
+				const prediction = model.predict(tf.tensor2d([[newPriceEach]]));
+				const [predictedEncodedProduct, predictedEncodedPurchaseAddress] = prediction.argMax(1).dataSync();
+
+				// console.log('Predicted Product:', decodeLength(predictedEncodedProduct, productCategories));
+				// console.log('Predicted Purchase Address:', decodeLength(predictedEncodedPurchaseAddress, purchaseAddressCategories));
+
+				res.json({
+					valuePredictProduct: decodeLength(predictedEncodedProduct, productCategories)
+				})
+			});
+		});	
+
+})
 
 app.listen(3001, () => {
   console.log('Server is running on http://localhost:3001');
